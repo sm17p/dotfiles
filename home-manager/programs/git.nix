c4b9f30636d61d41cc941e19e44e39dc740b33d8
Smit <smitp.contact@gmail.com>
2025-10-21 12:59:35 +0530

    update

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -1,7 +1,7 @@
 _: {
+  programs.difftastic.git.enable = true;
   programs.git = {
     enable = true;
-    difftastic.enable = true;
 
     ignores = [
       ".DS_Store"
@@ -9,16 +9,15 @@ _: {
       "/playwright/.cache/"
     ];
 
-    extraConfig = {
+    settings = {
       diff.algorithm = "histogram";
       init.defaultBranch = "main";
       log.date = "iso";
       merge.conflictstyle = "zdiff3";
       push.autoSetupRemote = true;
+      user.email = "smitp.contact@gmail.com";
+      user.name = "Smit";
     };
-
-    userEmail = "smitp.contact@gmail.com";
-    userName = "Smit";
   };
 
   programs.gh = {
